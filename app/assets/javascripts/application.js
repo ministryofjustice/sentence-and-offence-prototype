7664cd144eec1c863b9309500c12ec6e3677e7c1
ashley-wheat <ashley.wheat@digital.justice.gov.uk>
2022-03-21 13:28:25 +0000

    v8: first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,31 @@
+/* global $ */
+
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
+  window.MOJFrontend.initAll()
+})
+
+// Filter offences
+
+new MOJFrontend.FilterToggleButton({
+  bigModeMediaQuery: "(min-width: 48.063em)",
+  startHidden: false,
+  toggleButton: {
+    container: $(".moj-action-bar__filter"),
+    showText: "Show filter",
+    hideText: "Hide filter",
+    classes: "govuk-button--secondary",
+  },
+  closeButton: {
+    container: $(".moj-filter__header-action"),
+    text: "Close",
+  },
+  filter: {
+    container: $(".moj-filter"),
+  },
+});
