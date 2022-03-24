8d7e27aef40561dcdf4230c60e6ad141b9542362
ashley-wheat <ashley.wheat@digital.justice.gov.uk>
2022-03-24 14:08:25 +0000

    v1: tiled and sub menu version of overview pages added. Court case and offence info added

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Sentences and offences",
+  serviceName: "Sentence and offence information",
 
   // Default port that prototype runs on
   port: '3000',
