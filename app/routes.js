0599e910b5f6ea568879c3de203f47232c85a521
ashley-wheat <ashley.wheat@digital.justice.gov.uk>
2022-08-10 17:51:04 +0100

    v3: created an add offence journey

--- app/routes.js
+++ app/routes.js
@@ -15,7 +15,8 @@ router.post('/version-1/add-court-case/do-you-know-the-name-of-the-court', funct
     }
 })
 
-// Outcome type route
+// type of document route
+// version 3 add-a-remand-warrant
 router.post('/version-3/add-a-document/type-of-document-selected', function(req, res) {
 
     var typeOfDocument = req.session.data['type-of-document']
@@ -27,15 +28,32 @@ router.post('/version-3/add-a-document/type-of-document-selected', function(req,
     }
 })
 
-// //Type of document route
-// router.post('/version-3/add-a-document/type-of-document', function(req, res) {
+// add another offence choice
+// version 3 add-an-offence
+router.post('/version-3/add-an-offence/add-another-offence-selected', function(req, res) {
 
-//     var typeOfDocument = req.session.data['type-of-document']
+    var addAnotherOffence = req.session.data['add-another-offence-choice']
 
-//     console.log("typeOfDocument:" + typeOfDocument)
+    console.log("add-another-offence-choice" + addAnotherOffence)
 
-//     if (typeOfDocument == "remand-warrant") {
-//         res.redirect('/version-3/add-a-document/add-a-remand-warrant')
+    if (addAnotherOffence == "yes") {
+        res.redirect('/version-3/add-an-offence/add-an-offence')
+    }
+        if (addAnotherOffence == "no") {
+        res.redirect('/version-3/add-an-offence/check-answers')
+    }
+})
+
+// // add another offence choice
+// // version 3 add-an-offence
+// router.post('version-3/add-an-offence/add-another-offence-selected', function(req, res) {
+
+//     var addAnotherOffenceChoice = req.session.data['add-another-offence-choice']
+
+//     console.log("addAnotherOffenceChoice:" + addAnotherOffenceChoice)
+
+//     if (addAnotherOffenceChoice == "yes") {
+//         res.redirect('version-3/add-an-offence/add-an-offence')
 //     }
 // })
 
