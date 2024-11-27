95e49f4c3a19350f5df2bf1bd37144cb06fc38e5
Dbhudia17 <deepti.bhudia@digital.justice.gov.uk>
2024-11-27 16:40:21 +0000

    tsca-changes

--- app/routes.js
+++ app/routes.js
@@ -1,41 +1,30 @@
 const express = require('express')
-const router = express.Router()
-
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+// const router = express.Router()
 // Add your routes here - above the module.exports line
-
 // Court name route
 router.post('/version-1/add-court-case/do-you-know-the-name-of-the-court', function(req, res) {
-
     var knownCourtName = req.session.data['do-you-know-the-name-of-the-court']
-
     console.log("knownCourtName:" + knownCourtName)
-
     if (knownCourtName == "yes") {
         res.redirect('/version-1/add-court-case/search-by-name')
     }
 })
-
 // type of document route
 // version 3 add-a-remand-warrant
 router.post('/version-3/add-a-document/type-of-document-selected', function(req, res) {
-
     var typeOfDocument = req.session.data['type-of-document']
-
     console.log("type-of-document:" + typeOfDocument)
-
     if (typeOfDocument == "remand-warrant") {
         res.redirect('/version-3/add-a-document/add-a-remand-warrant/add-a-remand-warrant')
     }
 })
-
 // add another offence choice
 // version 3 add-an-offence
 router.post('/version-3/add-an-offence/add-another-offence-selected', function(req, res) {
-
     var addAnotherOffence = req.session.data['add-another-offence-choice']
-
     console.log("add-another-offence-choice" + addAnotherOffence)
-
     if (addAnotherOffence == "yes") {
         res.redirect('/version-3/add-an-offence/add-an-offence')
     }
@@ -43,15 +32,11 @@ router.post('/version-3/add-an-offence/add-another-offence-selected', function(r
         res.redirect('/version-3/add-an-offence/check-answers')
     }
 })
-
 // add another offence choice
 // version 3 add-a-warrant/add-an-offence
 router.post('/version-3/add-a-document/add-a-remand-warrant/add-an-offence/add-another-offence-selected', function(req, res) {
-
     var addAnotherOffence = req.session.data['add-another-offence-choice']
-
     console.log("add-another-offence-choice" + addAnotherOffence)
-
     if (addAnotherOffence == "yes") {
         res.redirect('/version-3/add-an-offence/add-an-offence')
     }
@@ -59,89 +44,51 @@ router.post('/version-3/add-a-document/add-a-remand-warrant/add-an-offence/add-a
         res.redirect('/version-3/add-a-document/add-a-remand-warrant/check-answers')
     }
 })
-
 router.post('/version-3/add-a-document/add-a-remand-warrant/hearing-type', function(req, res) {
-
     var nextHearingMonth = req.session.data['next-hearing-date-month']
     
     var date = monthToWord(nextHearingMonth);
-
     console.log("Next hearing month: " + date);
-
     req.session.data['next-hearing-date-month'] = date;
-
     res.redirect('/version-3/add-a-document/add-a-remand-warrant/hearing-type');
-
 })
-
 router.post('/version-3/add-a-document/add-a-remand-warrant/submit-court-date', function(req, res) {
-
     var courtDateMonth = req.session.data['court-date-month']
     
     var date = monthToWord(courtDateMonth);
-
     console.log("Court date month: " + date);
-
     req.session.data['court-date-month'] = date;
-
     res.redirect('/version-3/add-a-document/add-a-remand-warrant/add-an-offence/add-an-offence');
-
 })
-
-
-
 router.post('/version-3/add-an-offence/offence-end-date', function(req, res) {
-
     var offenceStartDate = req.session.data['offence-start-month']
     
     var date = monthToWord(offenceStartDate);
-
     console.log("Offence start month: " + date);
-
     req.session.data['offence-start-month'] = date;
-
     res.redirect('/version-3/add-an-offence/offence-end-date');
-
 })
-
-
 router.post('/version-3/add-a-document/add-a-remand-warrant/add-an-offence/offence-end-date', function(req, res) {
-
     var offenceStartDate = req.session.data['offence-start-month']
     
     var date = monthToWord(offenceStartDate);
-
     console.log("Offence start month: " + date);
-
     req.session.data['offence-start-month'] = date;
-
     res.redirect('/version-3/add-a-document/add-a-remand-warrant/add-an-offence/offence-end-date');
-
 })
-
 router.post('/version-3/add-a-document/add-a-remand-warrant/add-an-offence/add-another-offence', function(req, res) {
-
     var offenceEndDate = req.session.data['offence-end-month']
     
     var date = monthToWord(offenceEndDate);
-
     console.log("Offence end month name: " + date);
-
     req.session.data['offence-end-month'] = date;
-
     res.redirect('/version-3/add-a-document/add-a-remand-warrant/add-an-offence/add-another-offence');
-
 })
-
-
 // add another offence choice
 // version 3 add-a-warrant-for-custodial-sentence/add-a-sentence
 router.post('/version-3/add-a-document/add-a-warrant-for-custodial-sentence/add-a-sentence/add-another-sentence-selected', function(req, res) {
-
     var addAnotherOffence = req.session.data['add-another-offence-choice']
-
     console.log("add-another-offence-choice" + addAnotherOffence)
-
     if (addAnotherOffence == "yes") {
         res.redirect('/version-3/add-a-document/add-a-warrant-for-custodial-sentence/add-a-sentence/add-an-offence')
     }
@@ -149,10 +96,6 @@ router.post('/version-3/add-a-document/add-a-warrant-for-custodial-sentence/add-
         res.redirect('/version-3/add-a-document/add-a-warrant-for-custodial-sentence/check-answers')
     }
 })
-
-
-
-
 //Special remission - question
 router.post('special-remission-question', function (req, res) {
     var srselect = req.session.data['srselect']
@@ -168,29 +111,21 @@ router.post('special-remission-question', function (req, res) {
     }
   
   })
-
-
 //TSCA
 router.post('/tsca-question-submit', function (req, res) {
-    var radiobuttonname = req.session.data['tscaselect']
+    // this
+    var tscaselect = req.session.data['tscaselect']
    // Check whether the variable matches a condition
-    if (radiobuttonname == "Yes"){
+    if (tscaselect == "Yes"){
     // Send user to next page
-    res.redirect('/tsca-sentencing-date')}
+    res.redirect('/adjustments/beta/mvp-plus/TSCA/tsca-days')}
     else {
     // Send user to ppcs question
-    res.redirect('/tsca-question-ppcs')
+    res.redirect('/adjustments/beta/mvp-plus/TSCA/tsca-question-ppcs')
     }
-  
   })
-
-
-
-
 //convert date to words
-
 function monthToWord (monthNum){
-
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     var month = months[monthNum - 1]
   
@@ -199,18 +134,13 @@ function monthToWord (monthNum){
     return month;
   }
   
-
 // // add another offence choice
 // // version 3 add-an-offence
 // router.post('version-3/add-an-offence/add-another-offence-selected', function(req, res) {
-
 //     var addAnotherOffenceChoice = req.session.data['add-another-offence-choice']
-
 //     console.log("addAnotherOffenceChoice:" + addAnotherOffenceChoice)
-
 //     if (addAnotherOffenceChoice == "yes") {
 //         res.redirect('version-3/add-an-offence/add-an-offence')
 //     }
 // })
-
 module.exports = router
