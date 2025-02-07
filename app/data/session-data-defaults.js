78fb9320cf8551b0e7b6cb8c5224a7205b5654e5
jamesmckechnie <34271054+jamesmckechnie@users.noreply.github.com>
2025-02-07 11:19:21 +0000

    crds

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,23 +1,243 @@
-/*
-
-Provide default values for user session data. These are automatically added
-via the `autoStoreData` middleware. Values will only be added to the
-session if a value doesn't already exist. This may be useful for testing
-journeys where users are returning or logging in to an existing application.
-
-============================================================================
-
-Example usage:
-
-"full-name": "Sarah Philips",
-
-"options-chosen": [ "foo", "bar" ]
-
-============================================================================
-
-*/
-
 module.exports = {
+  courtCases: [ 
+    { 
+      "appearances": [{
+
+          "court-case-number": "4",
+          "overall-case-outcome": "Remanded in custody",
+          "next-court-date-set": "No",
+          "warrant-date-day": "03",
+          "warrant-date-month": "03",
+          "warrant-date-year": "2024",
+          "court-name": "	Abergavenny Magistrates Court",
+          // "source": "nomis",
+          "offences": [{
+                  "offence-start-date-day": "01",
+                  "offence-start-date-month": "03",
+                  "offence-start-date-year": "2024",
+                  "offence-end-date-day": "",
+                  "offence-end-date-month": "",
+                  "offence-end-date-year": "",
+                  "offence-code": [
+                      "HF46001"
+                  ],
+                  "offence-name": "HF46001 - Altering or defacing mark on sheep",
+                  "outcome": "Remand in custody",
+              } 
+          ]
+      },
+      { 
+
+          "court-case-number": "4",
+          "overall-case-outcome": "Imprisonment",
+          "overall-conviction-date-day": "30",
+          "overall-conviction-date-month": "04",
+          "overall-conviction-date-year": "2024",
+          "next-court-date-set": "No",
+          "warrant-date-day": "30",
+          "warrant-date-month": "04",
+          "warrant-date-year": "2024",
+          "court-name": "Abergavenny Magistrates Court",
+          "overall-sentence-length-years": "0",
+          "overall-sentence-length-months": "0",
+          "overall-sentence-length-weeks": "0",
+          "overall-sentence-length-days": "45",
+          // "status": ["saved","mismatch"],
+          "sentences": [{
+                  "count-number": "3",
+                  "conviction-date-day": "30",
+                  "conviction-date-month": "04",
+                  "conviction-date-year": "2024",
+                  "sentence-date-day": "30",
+                  "sentence-date-month": "04",
+                  "sentence-date-year": "2024",
+                  "offence-start-date-day": "01",
+                  "offence-start-date-month": "03",
+                  "offence-start-date-year": "2024",
+                  "offence-end-date-day": "",
+                  "offence-end-date-month": "",
+                  "offence-end-date-year": "",
+                  "offence-code": [
+                      "HF46001"
+                  ],
+                  "offence-name": "HF46001 - Altering or defacing mark on sheep",
+                  "outcome": "Imprisonment",
+                  "sentence-type": "SDS (Standard Determinate Sentence)",
+                  "sentence-length-years": "0",
+                  "sentence-length-months": "0",
+                  "sentence-length-weeks": "0",
+                  "sentence-length-days": "45",
+                  "consecutive-concurrent": "Concurrent",
+                  "status": "active",
+    
+              } 
+          ]
+      }
+  ]
+  },
+      { 
+          "appearances": [{
+
+              "court-case-number": "3",
+              "overall-case-outcome": "Remanded in custody",
+              "next-court-date-set": "No",
+              "warrant-date-day": "03",
+              "warrant-date-month": "03",
+              "warrant-date-year": "2024",
+              "court-name": "	Aberdare County Court",
+              // "source": "nomis",
+              "offences": [{
+                      "offence-start-date-day": "09",
+                      "offence-start-date-month": "02",
+                      "offence-start-date-year": "2024",
+                      "offence-end-date-day": "",
+                      "offence-end-date-month": "",
+                      "offence-end-date-year": "",
+                      "offence-code": [
+                          "TP47033"
+                      ],
+                      "offence-name": "TP47033 - Drive cattle furiously in the street",
+                      "outcome": "Remand in custody",
+                  } 
+              ]
+          },
+          { 
+
+              "court-case-number": "3",
+              "overall-case-outcome": "Imprisonment",
+              "overall-conviction-date-day": "09",
+              "overall-conviction-date-month": "02",
+              "overall-conviction-date-year": "2024",
+              "next-court-date-set": "No",
+              "warrant-date-day": "09",
+              "warrant-date-month": "02",
+              "warrant-date-year": "2024",
+              "court-name": "	Aberdare County Court",
+              "overall-sentence-length-years": "0",
+              "overall-sentence-length-months": "0",
+              "overall-sentence-length-weeks": "0",
+              "overall-sentence-length-days": "136",
+              // "status": ["saved","mismatch"],
+              "sentences": [{
+                      "count-number": "2",
+                      "conviction-date-day": "09",
+                      "conviction-date-month": "02",
+                      "conviction-date-year": "2024",
+                      "sentence-date-day": "09",
+                      "sentence-date-month": "02",
+                      "sentence-date-year": "2024",
+                      "offence-start-date-day": "01",
+                      "offence-start-date-month": "02",
+                      "offence-start-date-year": "2024",
+                      "offence-end-date-day": "",
+                      "offence-end-date-month": "",
+                      "offence-end-date-year": "",
+                      "offence-code": [
+                          "TP47033"
+                      ],
+                      "offence-name": "TP47033 - Drive cattle furiously in the street",
+                      "outcome": "Imprisonment",
+                      "sentence-type": "SDS (Standard Determinate Sentence)",
+                      "sentence-length-years": "0",
+                      "sentence-length-months": "0",
+                      "sentence-length-weeks": "0",
+                      "sentence-length-days": "136",
+                      "consecutive-concurrent": "Concurrent",
+                      "status": "active",
+     
+                  } 
+              ]
+          }
+      ]
+      },
+      {   
+          "appearances": [{
+                  "court-case-ref": "T20221234",
+                  "court-case-number": "1",
+                  "overall-case-outcome": "Remand in custody",
+                  "next-court-date-set": "No",
+                  "warrant-date-day": "20",
+                  "warrant-date-month": "06",
+                  "warrant-date-year": "2024",
+                  "court-name": "	Snaresbrook Crown Court",
+                  // "source": "nomis",
+                  "offences": [{
+                          "offence-start-date-day": "29",
+                          "offence-start-date-month": "10",
+                          "offence-start-date-year": "2023",
+                          "offence-end-date-day": "",
+                          "offence-end-date-month": "",
+                          "offence-end-date-year": "",
+                          "offence-code": [
+                              "OF61102"
+                          ],
+                          "offence-name": "OF61102 - Assault a person thereby occasioning them actual bodily harm",
+                          "outcome": "Remand in custody",
+                      } 
+                  ]
+              },
+              {
+                  "court-case-ref": "T20221234",
+                  "court-case-number": "1",
+                  "overall-case-outcome": "Imprisonment",
+                  "overall-conviction-date-day": "08",
+                  "overall-conviction-date-month": "09",
+                  "overall-conviction-date-year": "2024",
+                  "next-court-date-set": "No",
+                  "warrant-date-day": "08",
+                  "warrant-date-month": "09",
+                  "warrant-date-year": "2024",
+                  "court-name": "	Snaresbrook Crown Court",
+                  "overall-sentence-length-years": "3",
+                  "overall-sentence-length-months": "0",
+                  "overall-sentence-length-weeks": "0",
+                  "overall-sentence-length-days": "0",
+                  // "status": ["saved","mismatch"],
+                  "sentences": [{
+                          "count-number": "4",
+                          "conviction-date-day": "08",
+                          "conviction-date-month": "09",
+                          "conviction-date-year": "2024",
+                          "sentence-date-day": "08",
+                          "sentence-date-month": "09",
+                          "sentence-date-year": "2024",
+                          "offence-start-date-day": "29",
+                          "offence-start-date-month": "10",
+                          "offence-start-date-year": "2023",
+                          "offence-end-date-day": "",
+                          "offence-end-date-month": "",
+                          "offence-end-date-year": "",
+                          "offence-code": [
+                              "OF61102"
+                          ],
+                          "offence-name": "OF61102 - Assault a person thereby occasioning them actual bodily harm",
+                          "outcome": "Imprisonment",
+                          "sentence-type": "SDS (Standard Determinate Sentence)",
+                          "sentence-length-years": "3",
+                          "sentence-length-months": "0",
+                          "sentence-length-weeks": "0",
+                          "sentence-length-days": "0",
+                          "consecutive-concurrent": "Forthwith",
+                          "status": "active",
+                      }
+                  ]
+              }
+          ]
+      }
+      
+  ]
+  
+  // ,courtCase: {
+  //     'court-case-ref': '123',
+  //     'court-name': 'Birmingham Court',
+  //     'warrant-date-day': '12',
+  //     'warrant-date-month': '9',
+  //     'warrant-date-year': '2023',
+  //     'type-of-case': "Remand",
+  //     'overall-case-outcome': "Remanded in custody",
+  //     'next-court-date-set': "No",
+  //      offences: []
+  // } Example of what is stored in the session for submitting a court case
 
   // Insert values here
 
